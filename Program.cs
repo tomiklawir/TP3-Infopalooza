b98178b0d546afb1c842190bdb291dd4b06d9aeb
TOMAS DAVID KLAWIR <48385206@ort>
2024-05-08 08:59:04 -0300

    Case 1 del switch casi terminado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿const int NUEVA_INSCRIPCION = 1, OBTENER_ESTADISTICAS = 2, BUSCAR_CLIENTE = 3, CAMBIAR_ENTRADA = 4, SALIR = 5;
-int opcion;
+int opcion, dni, tipoEntrada, cantidad, idCliente = 10000;
+string nombre, apellido;
+DateTime fechaInscripcion = new DateTime();
 
 do{
     opcion = IngresarNumero("Ingrese un numero: NUEVA INSCRIPCION = 1, OBTENER ESTADISTICAS = 2, BUSCAR CLIENTE = 3, CAMBIAR ENTRADA = 4, SALIR = 5");
@@ -8,7 +10,13 @@ do{
 while(opcion != 5)
 {   switch(opcion)
     {
-        case NUEVA_INSCRIPCION: 
+        case NUEVA_INSCRIPCION:
+        dni = IngresarNumero("Ingrese su dni");
+        apellido = IngresarString("Ingrese su apellido");
+        nombre = IngresarString("Ingrese su nombre");
+        fechaInscripcion = IngresarFecha("Ingrese la fecha de inscripcion");
+
+        cantidad = IngresarNumero("Ingrse la cantidad de entradas");
         break;
 
         case OBTENER_ESTADISTICAS: 
@@ -25,7 +33,28 @@ while(opcion != 5)
 
 static int IngresarNumero(string mensaje)
 {
-Console.WriteLine(mensaje);
-int num= int.Parse(Console.ReadLine());
-return num;
+    int num;
+    do{
+        Console.WriteLine(mensaje);
+        num= int.Parse(Console.ReadLine());
+    }while(num < 0);
+    return num;
+}
+
+static string IngresarString(string mensaje){
+    string cadena;
+    do{
+        Console.WriteLine(mensaje);
+        cadena = Console.ReadLine();
+    }while(cadena == string.Empty);
+    return cadena;
+}
+
+static DateTime IngresarFecha(string mensaje){
+    DateTime fecha = new DateTime();
+    do{
+        Console.WriteLine(mensaje);
+        fecha = DateTime.Parse(Console.ReadLine());
+    }while(fecha == null);
+    return fecha;
 }
